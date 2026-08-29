2b3b625d3322d1d9a4af366d97e102aa90a0923e
anchen <anchenkru@gmail.com>
2026-08-29 22:55:38 +0200

    Done some of performance, revisit

--- include/composite/PerformanceStage.h
+++ include/composite/PerformanceStage.h
@@ -9,20 +9,59 @@ private:
 	std::vector<std::string> schedule;
 	std::string currentPerformance;
 	bool isPerforming;
+	bool isOpen;
 	std::string currentStageManager;
 
 public:
+	/**
+	 * @brief The constructor for the Performance stage object
+	 *
+	 * @param name of the performance
+	 * @param capacity of the performance
+	 */
 	PerformanceStage(std::string name, int capacity);
+
+	/**
+	 * @brief Starting the performance based on certain conditions
+	 *
+	 * @param show the current show that wants to start
+	 */
 	void startPerformance(std::string show);
+
+	/**
+	 * @brief pausing the performance it has not completed yet
+	 */
 	void pausePerformance();
+
+	/**
+	 * @brief change the current manager to another manager
+	 *
+	 * @param newManager the manager that will be changed to be the current manager
+	 */
 	void changeStageManager(std::string newManager);
 
 	// event unit
 	void handleNotice(Notice *notice) override;
 	// event component
+
+	/**
+	 * @brief Open up the stage to be performed on
+	 */
 	void open() override;
+
+	/**
+	 * @brief Close the stage as there can no longer be performed on
+	 */
 	void close() override;
+
+	/**
+	 * @brief General overview of the performance
+	 */
 	void reportStatus() override;
+
+	/** 
+	 * @brief the capacity that the performance is able to take
+	 */
 	int getCapacity() override;
 };
 
--- src/composite/PerformanceStage .cpp
+++ src/composite/PerformanceStage .cpp
@@ -1,21 +1,46 @@
 #include "../../include/composite/PerformanceStage.h"
 
-PerformanceStage::PerformanceStage(std::string name, int capacity)
+PerformanceStage::PerformanceStage(std::string name, int capacity) : EventUnit(name, true, capacity), currentPerformance(""), isPerforming(false), currentStageManager("")
 {
 }
 
 void PerformanceStage::startPerformance(std::string show)
 {
+    if (currentStageManager == "")
+    {
+        std::cout << "No manager, no performance!" << std::endl;
+    }
+    else
+    {
+        isPerforming = true;
+        return;
+    }
 }
 
 void PerformanceStage::pausePerformance()
 {
+    if (!isPerforming)
+    {
+        std::cout << "Crickets.... there is no performance at the moment" << std::endl;
+        return;
+    }
+    std::cout << "Stop the " << name << "performance!! I object!" << std::endl;
+    isPerforming = false;
 }
 
 void PerformanceStage::changeStageManager(std::string newManager)
 {
+    if (isPerforming)
+    {
+        std::cout << "Hold your horses, wait for the performance to stop or pause it before you switch out a manager." << std::endl;
+        return;
+    }
+
+    std::cout << "Bye bye, " << currentStageManager << " helloooo " << newManager << std::endl;
+    currentStageManager = newManager;
 }
 
+// observer
 void PerformanceStage::handleNotice(Notice *notice)
 {
 }
@@ -23,10 +48,14 @@ void PerformanceStage::handleNotice(Notice *notice)
 // event component
 void PerformanceStage::open()
 {
+    std::cout << "Now opening........" << name << ", everyone celebrate!" << std::endl;
+    isOpen = true;
 }
 
 void PerformanceStage::close()
 {
+
+    isOpen = false;
 }
 
 void PerformanceStage::reportStatus()
@@ -35,4 +64,5 @@ void PerformanceStage::reportStatus()
 
 int PerformanceStage::getCapacity()
 {
+    return EventUnit::getCapacity();
 }
